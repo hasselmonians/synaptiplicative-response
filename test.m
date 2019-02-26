70a6de29412d1f03e8fd9ef9b56360e41385b09d
Alec Hoyland <alec.hoyland@posteo.net>
2019-02-26 17:34:00 -0500

    Simplify test function for now

--- test.m
+++ test.m
@@ -1,24 +1,29 @@
 % test a simple model of the dendrite based on Behabadi et al. 2012
 % proximal-distal, time-invariant 2-compartment model
 
-% preamble
-cond_scale = 1;
-
 % instantiate the xolotl object
 x = xolotl;
 
 % create the compartments
-x.add('compartment', 'prox', 'Cm', 10, 'radius', 0.001, 'len', 0.010);
-x.add('compartment', 'dist', 'Cm', 10, 'radius', 0.001, 'len', 0.010);
+x.add('compartment', 'proximal', 'Cm', 10, 'A', 0.01);
+% x.add('compartment', 'distal', 'Cm', 10, 'radius', 0.001, 'len', 0.010);
 x.add('compartment', 'presynaptic');
 
 % add leak currents
-x.prox.add('Leak', 'gbar', @() 4*cond_scale, 'E', -70);
-x.dist.add('Leak', 'gbar', @() 4*cond_scale, 'E', -70);
+x.proximal.add('Leak', 'gbar', 0, 'E', -70);
+% x.distal.add('Leak', 'gbar', @() 4, 'E', -70);
 
 % connect the compartments
-x.connect('prox', 'dist', 'gmax', @() 2.5*cond_scale);
+% x.connect('proximal', 'distal', 'Axial', 'gmax', @() 2.5);
 
 % add synapses from controlling compartment to proximal and distal compartments
-x.connect('presynaptic', 'prox', 'borgers/NMDAergic', 'gmax', @() 0.5*cond_scale, 'E', 0);
-x.connect('presynaptic', 'dist', 'borgers/NMDAergic', 'gmax', @() 0.0*cond_scale, 'E', 0);
+x.connect('presynaptic', 'proximal', 'borgers/NMDAergic', 'gmax', 100, 'E', 0);
+% x.connect('presynaptic', 'distal', 'borgers/NMDAergic', 'gmax', @() 0.0, 'E', 0);
+
+% voltage clamp the presynaptic compartment
+V_clamp = NaN * x.integrate;
+nSteps = x.t_end / x.dt;
+spike_start = round(0.2*nSteps);
+spike_stop = spike_start + spike_start;
+V_clamp(:,1) = -60;
+V_clamp(spike_start:spike_stop, 1) = 50;
