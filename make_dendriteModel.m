be139d3f903830bcb531584d6fff9ed28514251d
Alec Hoyland <alec.hoyland@posteo.net>
2019-03-11 14:07:26 -0400

    Add presynaptic compartments and NMDA synapses

--- make_dendriteModel.m
+++ make_dendriteModel.m
@@ -1,10 +1,16 @@
-function x = make_dendriteModel(N)
+function x = make_dendriteModel(N, postsynaptic)
     % produces a model of CA1/CA3 dendrites in xolotl
+    %
     % N is the number of compartments
+    %
+    % postsynaptic is a 2x1 cell array containing character vector indices
+    % to which compartments should be postsynaptic to the NMDA synapses
+    %
     % measurements cited in Traub et al. 1991
     % based on Turner & Schwartzkroin 1980, 1983; & others
 
     %% Instantiate the xolotl object
+
     x = xolotl;
 
     %% Instantiate compartments and conductances
@@ -25,3 +31,14 @@ function x = make_dendriteModel(N)
 
     % slice the compartments into N sub-compartments
     x.slice('Dendrite', N);
+
+    %% Add presynaptic compartments
+
+    % this compartment will be voltage-clamped so it doesn't need any conductances
+    x.add('compartment', 'Presynaptic1', 'Cm', 30, 'Cm', 30, 'radius', 2.42/1e3, 'len', 110/1e3)
+    x.add('compartment', 'Presynaptic2', 'Cm', 30, 'Cm', 30, 'radius', 2.42/1e3, 'len', 110/1e3)
+
+    %% Connect presynaptic compartments to postsynaptic compartments
+
+    x.connect('Presynaptic1', ['Dendrite', postsynaptic{1}], 'borgers/NMDAergic', 'gbar', 0, 'Mg', 2, 'tau_d', 2, 'tau_r', 10);
+    x.connect('Presynaptic2', ['Dendrite', postsynaptic{2}], 'borgers/NMDAergic', 'gbar', 0, 'Mg', 2, 'tau_d', 2, 'tau_r', 10);
