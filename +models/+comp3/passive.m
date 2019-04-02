04b80af4cb9d73352861c9af14a762be4d31b9af
Alec Hoyland <alec.hoyland@posteo.net>
2019-04-02 11:43:47 -0400

    Add 3-compartment passive model

---
+++ +models/+comp3/passive.m
@@ -0,0 +1,22 @@
+x = xolotl;
+N = 3;
+
+%% Instantiate compartments and conductances
+
+x.add('compartment', 'Dendrite', 'Cm', 30, 'radius', 4e-3, 'len', N * 110e-3);
+
+% add only a leak current
+x.Dendrite.add('Leak', 'gbar', 0.1, 'E', -50);
+
+% slice the compartment
+x.slice('Dendrite', N);
+
+%% Add presynaptic compartments
+
+x.add('compartment', 'Presynaptic1', 'Cm', 30, 'radius', 4e-3, 'len', 110e-3);
+x.add('compartment', 'Presynaptic2', 'Cm', 30, 'radius', 4e-3, 'len', 110e-3);
+
+%% Connect presynaptic compartments to postsynaptic compartments
+
+x.connect('Presynaptic1', 'Dendrite1', 'borgers/NMDAergic', 'gmax', 0, 'Mg', 2, 'tau_d', 2, 'tau_r', 10);
+x.connect('Presynaptic2', 'Dendrite3', 'borgers/NMDAergic', 'gmax', 0, 'Mg', 2, 'tau_d', 2, 'tau_r', 10);
