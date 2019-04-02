dd4c35454616db8e1ec3dbe45488002398502c43
Alec Hoyland <alec.hoyland@posteo.net>
2019-04-02 12:38:28 -0400

    Convert to function

--- +models/+comp2/+passive/model.m
+++ +models/+comp2/+passive/model.m
@@ -1,22 +1,26 @@
-x = xolotl;
-N = 2;
+function x = model()
+  
+  x = xolotl;
+  N = 2;
 
-%% Instantiate compartments and conductances
+  %% Instantiate compartments and conductances
 
-x.add('compartment', 'Dendrite', 'Cm', 30, 'radius', 4e-3, 'len', N * 110e-3);
+  x.add('compartment', 'Dendrite', 'Cm', 30, 'radius', 4e-3, 'len', N * 110e-3);
 
-% add only a leak current
-x.Dendrite.add('Leak', 'gbar', 0.1, 'E', -50);
+  % add only a leak current
+  x.Dendrite.add('Leak', 'gbar', 0.1, 'E', -50);
 
-% slice the compartment
-x.slice('Dendrite', N);
+  % slice the compartment
+  x.slice('Dendrite', N);
 
-%% Add presynaptic compartments
+  %% Add presynaptic compartments
 
-x.add('compartment', 'Presynaptic1', 'Cm', 30, 'radius', 4e-3, 'len', 110e-3);
-x.add('compartment', 'Presynaptic2', 'Cm', 30, 'radius', 4e-3, 'len', 110e-3);
+  x.add('compartment', 'Presynaptic1', 'Cm', 30, 'radius', 4e-3, 'len', 110e-3);
+  x.add('compartment', 'Presynaptic2', 'Cm', 30, 'radius', 4e-3, 'len', 110e-3);
 
-%% Connect presynaptic compartments to postsynaptic compartments
+  %% Connect presynaptic compartments to postsynaptic compartments
 
-x.connect('Presynaptic1', 'Dendrite1', 'borgers/NMDAergic', 'gmax', 0, 'Mg', 2, 'tau_d', 2, 'tau_r', 10);
-x.connect('Presynaptic2', 'Dendrite2', 'borgers/NMDAergic', 'gmax', 0, 'Mg', 2, 'tau_d', 2, 'tau_r', 10);
+  x.connect('Presynaptic1', 'Dendrite1', 'borgers/NMDAergic', 'gmax', 0, 'Mg', 2, 'tau_d', 2, 'tau_r', 10);
+  x.connect('Presynaptic2', 'Dendrite2', 'borgers/NMDAergic', 'gmax', 0, 'Mg', 2, 'tau_d', 2, 'tau_r', 10);
+
+end
