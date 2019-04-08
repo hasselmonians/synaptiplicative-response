1e015e43f16e3703d867b6d655b21d2eb7457d21
Alec Hoyland <alec.hoyland@posteo.net>
2019-04-08 15:05:23 -0400

    Increase upper bound for parameters

--- models/+comp2/+passive/optimize.m
+++ models/+comp2/+passive/optimize.m
@@ -28,7 +28,7 @@ p.sim_func  = @comp2.simulate;
 % parameters
 p.parameter_names = [x.find('*gbar'); x.find('*NMDAergic*gmax')];
 p.lb        = zeros(1, length(p.parameter_names));
-p.ub        = 300 * ones(1, length(p.parameter_names)); % uS/mm^2
+p.ub        = 1000 * ones(1, length(p.parameter_names)); % uS/mm^2
 
 % set procrustes options
 p.options.MaxTime   = 900;
