41abbbf590d15089e0ce6a1d2b54c49c4561dbd5
Alec Hoyland <alec.hoyland@posteo.net>
2019-04-19 18:38:35 -0400

    Set lambda to zero across the board

--- models/+comp1/simulate.m
+++ models/+comp1/simulate.m
@@ -17,7 +17,7 @@ function [cost, costParts, R] = simulate(x, ~, ~)
   % useful variables
   comps       = x.find('compartment');  % list of all compartments
   epsilon     = 0.01;                   % mV, minimum allowed response
-  lambda      = [1, 0, 0];              % weighting of multiplicative, additive, and supralinear costs
+  lambda      = [0, 0, 0];              % weighting of multiplicative, additive, and supralinear costs
 
   % set up presynaptic waveform pulse
   pulseWidth  = round(2 / x.dt);
