09fdc1ac9e9891486ce4640b5a54de7e26d2ba81
Alec Hoyland <alec.hoyland@posteo.net>
2019-04-23 18:16:40 -0400

    Add manipulate to all comp1 models

--- models/+comp1/+transient/manip.m
+++ models/+comp1/+transient/manip.m
@@ -1,7 +1,7 @@
 % creates a manipulation figure that allows for manual optimization/exploration
 % of the parameter space
 
-x = comp1.passive.model();
+x = comp1.transient.model();
 x.manipulate_plot_func = {@comp1.manip_func};
 
 x.manipulate([x.find('*gbar'); x.find('*NMDAergic*gmax')])
