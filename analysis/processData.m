47747acd5d1950b909af6356f47bdfd14cd97505
Alec Hoyland <alec.hoyland@posteo.net>
2019-04-24 14:05:07 -0400

    Add function to gather data and preprocess

---
+++ analysis/processData.m
@@ -0,0 +1,30 @@
+function [dataTable, param_names] = processData(filepaths)
+  % gathers data from .mat files produced by simulations
+  % that match the pattern in `filepaths`
+  % and performs some filtering
+  %
+  % Ex: processData('data-comp1-passive*')
+  %
+  % the data are expected to have cost, costParts, params, and responses as fields
+
+  dirs = dir(filepaths);
+  assert(numel(dirs) > 0, ['could not find any data files for filepath: ' filepaths])
+
+  % load the first dataset
+  data = load(dirs(1).name);
+  % save the parameter names separately, since it is sized differently
+  param_names = data.param_names;
+  data = rmfield(data, 'param_names');
+  % create a table from the struct directly
+  dataTable = struct2table(data);
+
+  % for each other dataset, append
+  if length(dirs) > 1
+    for ii = 2:length(dirs)
+      data = load(dirs(ii).name);
+      data = rmfield(data, 'param_names');
+      dataTable2 = struct2table(data);
+      dataTable = [dataTable; dataTable2];
+    end
+  end
+end % function
