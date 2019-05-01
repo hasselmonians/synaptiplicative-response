381ef36f615e69cd535ccc72487ae3fd719df3e6
Alec Hoyland <alec.hoyland@posteo.net>
2019-05-01 16:51:48 -0400

    Add optimize scripts for spiking and transient conditions

---
+++ models/+comp1/+transient/optimize.m
@@ -0,0 +1,83 @@
+% parameter optimization script for 1-compartment transient models
+% with no extra special conditions.
+
+% ## Experimental protocol
+%
+% * Reach steady-state.
+% * Zap presynaptic compartment #1.
+% * Observe EPSPs in postsynaptic compartment #1.
+% * Return to steady-state.
+% * Zap presynaptic compartment #2
+% * Observe EPSPs in postsynaptic compartment #1.
+% * Return to steady-state.
+% * Zap presynaptic compartments #1 & #2
+% * Observe EPSPs in postsynaptic compartment #1.
+%
+% Optimize for the condition where EPSP #1 + EPSP #2 != EPSP #1 & EPSP #2.
+% That is, that there is a nonlinear "multiplicative" response.
+
+%% Set up xolotl and xfit
+
+x           = comp1.transient.model();
+x.t_end     = 100; % ms, only need to record one EPSP
+
+p           = xfit('particleswarm');
+p.x         = x;
+p.sim_func  = @comp1.simulate;
+
+% parameters
+param_names = [x.find('Dendrite*gbar'); x.find('Dendrite*NMDAergic*gmax')];
+p.parameter_names = param_names;
+p.lb        = zeros(1, length(p.parameter_names));
+p.ub        = 30 * ones(1, length(p.parameter_names)); % uS/mm^2
+
+% set procrustes options
+p.options.MaxTime   = 900;
+p.options.SwarmSize = 24;
+
+%% Initialize outputs
+
+nSims       = 100;
+nEpochs     = 3;
+nParams     = length(p.parameter_names);
+params      = NaN(nSims, nParams);
+cost        = NaN(nSims, 1);
+costParts   = NaN(nSims, 4);
+responses   = NaN(nSims, 3);
+
+%% Fit parameters
+
+filename    = ['data-comp1-transient-' corelib.getComputerName '.mat'];
+
+if exist(filename)
+  load(filename)
+  start_idx = find(isnan(cost),1,'first')
+else
+  start_idx = 1;
+end
+
+for ii = start_idx:nSims
+
+  try
+
+    % set seed
+    p.seed = p.ub - 0.5 * p.ub .* rand(size(p.ub)); % start with high parameters > 0.5 * p.ub
+
+    % run xfit
+    for qq = 1:nEpochs
+      p.fit;
+    end
+
+    % save
+    params(ii, :)  = p.seed;
+    [cost(ii), costParts(ii, :), responses(ii, :)] = p.sim_func(x);
+    save(filename, 'cost', 'params', 'costParts', 'responses', 'param_names');
+    disp(['saved simulation ' num2str(ii)])
+
+  catch
+
+    disp('Something went wrong.')
+
+  end
+
+end
