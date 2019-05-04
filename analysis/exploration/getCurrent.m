d12c55e810f7539a739342a62c1b5799a9ae262f
Alec Hoyland <alec.hoyland@posteo.net>
2019-05-04 16:17:23 -0400

    Plot gating functions for NMDA

--- analysis/exploration/getCurrent.m
+++ analysis/exploration/getCurrent.m
@@ -1,7 +1,76 @@
-function I = getCurrent(V_pre, V_post)
+function getCurrent(V_pre, V_post)
   % computes the steady-state current as a function of presynaptic and postsynaptic membrane potential
   % uses equations for the Borgers/Jahr-Stevens NMDA synapse
 
-  [s_inf, tau_r, u] = getGatingFunctions();
+  [s_inf, tau_s, u] = getGatingFunctions();
 
   % TODO: plot steady-state, ss * u, and ss * u * (V - E)
+
+  % evaluate these functions
+  V = linspace(-100,100,1e3);
+  sinf = NaN*V;
+  taus = NaN*V;
+  uval = NaN*V;
+
+  % compute the gating functions/time constants
+  for ii = 1:length(V)
+    sinf(ii) = s_inf(V(ii));
+    taus(ii) = tau_s(V(ii));
+    uval(ii) = u(V(ii));
+  end
+
+  %% Plot the figure
+  % 2x2 grid
+
+  f = figure('outerposition',[100 100 1000 900],'PaperUnits','points','PaperSize',[1000 500]); hold on
+  for ii = 4:-1:1
+    ax(ii) = subplot(2,2,ii); hold on
+  end
+
+  ax(1).Tag = 's_inf';
+  ax(2).Tag = 'tau_s';
+  ax(3).Tag = 's_inf*u';
+  ax(4).Tag = 's_inf*u*(V-E)';
+
+  % plot the gating function and time constant
+
+  plot(ax(1), V, sinf, 'k');
+  ylabel(ax(1),'s_{\infty}')
+  xlabel(ax(1),'V (mV)')
+  set(ax(1),'YLim',[0 1])
+
+  plot(ax(2), V, uval, 'k');
+  ylabel(ax(2),'u')
+  xlabel(ax(2),'V (mV)')
+  set(ax(2),'YLim',[0 1])
+
+  plot(ax(3), V, taus, 'k');
+  xlabel(ax(3),'V (mV)')
+  ylabel(ax(3),'\tau_{s} (ms)')
+  % set(ax(3),'YScale','log')
+
+  % plot the normalized current as a function of both V_pre and V_post
+  Vmesh = meshgrid(V);
+  I = zeros(size(Vmesh));
+  for ii = 1:length(V)
+    for qq = 1:length(V)
+      I(ii, qq) = sinf(ii) * uval(qq) * V(qq); % s * u * (V - E), where E = 0
+    end
+  end
+
+  imagesc(ax(4), V, V, I');
+  ylabel(ax(4),'V_{post} (mV)')
+  xlabel(ax(4),'V_{pre} (mV)')
+  % set(ax(4),'YScale','log')
+  c = colorbar(ax(4));
+  c.Label.String = 'norm. current density (nA/mm^2)';
+
+  try
+  	figlib.pretty();
+  catch
+  end
+
+  % turn all YLim modes to auto
+  for i = 1:length(ax)
+  	ax(i).YLimMode = 'auto';
+  end
--- analysis/exploration/getGatingFunctions.m
+++ analysis/exploration/getGatingFunctions.m
@@ -1,16 +1,8 @@
-function [s_inf, tau_s, u] = getGatingFunctions(varargin)
-
+function [s_inf, tau_s, u] = getGatingFunctions()
   % get the gating functions for Borgers/Jahr-Stevens NMDA model
 
-  options = struct;
-  options.Mg = 1;
-  options.tau_r = 2;
-  options.tau_d = 10;
-
-  corelib.parseNameValueArguments(options, varargin{:});
-
-  s_inf = @(v) ((1 + tanh(v/10))/2) / ( ((1 + tanh(v/10))/2) + options.tau_r/options.tau_d );
-  tau_s = @(v) tau_r / ( ((1 + tanh(v/10))/2) + options.tau_r/options.tau_d );
-  u     = @(v) 1 / ( 1 + options.Mg/3.57 * exp(-0.062 * v) );
+  s_inf = @(v) ((1 + tanh(v/10))/2) / ( ((1 + tanh(v/10))/2) + 2/10 );
+  tau_s = @(v) 2 / ( ((1 + tanh(v/10))/2) + 2/10 );
+  u     = @(v) 1 / ( 1 + 1/3.57 * exp(-0.062 * v) );
 
 end % function
