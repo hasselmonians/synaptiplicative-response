ab02c7157af05531fdacfa3f424b97ea63dbdb04
Alec Hoyland <alec.hoyland@posteo.net>
2019-05-13 14:45:14 -0400

    Only plot one curve

--- analysis/plotIV.m
+++ analysis/plotIV.m
@@ -1,41 +1,30 @@
-function plotIV(ax, sinf, u)
-  % make a plot of steady-state NMDAergic synaptic current vs. presynaptic membrane potential
-  % parametrized by postsynaptic membrane potential
+function plotIV(ax, u)
+  % make a plot of steady-state NMDAergic synaptic current vs. postsynaptic membrane potential
+  % parametrized by presynaptic membrane potential
 
   % get the voltage and gating functions
-  Vpre    = linspace(-20, 100, 21);
-  Vpost   = linspace(-100, 100, 1e3+1);
+  Vpost   = linspace(-70, 0, 1e3+1);
 
   if nargin < 2
-    [sinf, ~, u] = getGatingFunctions();
-  end
-
-  if nargin < 3
     [~, ~, u] = getGatingFunctions();
   end
 
-  % compute the current as a matrix
-  I       = zeros(length(Vpost), length(Vpre));
+  % compute the current
+  I       = zeros(length(Vpost), 1);
   for ii = 1:length(Vpost)
-    for qq = 1:length(Vpre)
-      I(ii, qq) = sinf(Vpre(qq)) * u(Vpost(ii)) * Vpost(ii);
-    end
+      I(ii) = u(Vpost(ii)) * Vpost(ii);
   end
 
   %% Plot the figure
-  % 1x1 with many overlaid plots
+  % 1x1
 
   if nargin < 1 || isempty(ax)
     figure('outerposition',[100 100 1000 800],'PaperUnits','points','PaperSize',[1000 800]); hold on
-    ax = axes;
+    ax = gca;
   end
-
-  C = colormaps.linspecer(length(Vpre));
 
   hold(ax, 'on');
-  for ii = 1:size(I, 2)
-    plot(ax, Vpost, I(:, ii), 'Color', C(ii, :));
-  end
+    plot(ax, Vpost, I, 'k');
 
   xlabel(ax, 'postsynaptic membrane potential (mV)')
   ylabel(ax, 'norm. current density (nA/mm^2)')
@@ -43,12 +32,7 @@ function plotIV(ax, sinf, u)
   ymin = min(I(:)) - 0.1 * yoffset;
   ymax = max(I(:)) + 0.1 * yoffset;
   ylim(ax, [ymin ymax])
-
-  c = colorbar(ax, 'Location', 'EastOutside');
-  c.Label.String = 'presynaptic membrane potential (mV)';
-  c.Ticks = Vpre;
-  caxis(ax, [min(Vpre) max(Vpre)]);
-  colormap(ax, colormaps.linspecer)
+  title('NMDAergic peak current IV curve')
 
   try
   	figlib.pretty();
