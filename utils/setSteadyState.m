52be4a57ee6b2a3a0674dee9cf66213972962374
Alec Hoyland <alec.hoyland@posteo.net>
2019-05-14 15:45:31 -0400

    Don't clamp, find steady-state naturally

--- utils/setSteadyState.m
+++ utils/setSteadyState.m
@@ -1,7 +1,6 @@
 function x = setSteadyState(x)
   % compute the steady-state and save as a snapshot
   x.t_end = 5e3;
-  x.V_clamp = [NaN, -60, -60];
   V = x.integrate;
   x.t_end = 30;
   x.snapshot('steady-state');
