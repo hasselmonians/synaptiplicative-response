6506b233db0f5dffaa45d3c3cd320dc542c1b660
Alec Hoyland <alec.hoyland@posteo.net>
2019-05-14 16:00:38 -0400

    Set hyperpolarization holding potential at -70

--- double_pulse_experiment/+comp1/simulate_core.m
+++ double_pulse_experiment/+comp1/simulate_core.m
@@ -9,17 +9,17 @@ function [r, V] = simulation_core(x, comps, trial, pulseStart, pulseStop, pulseH
   switch trial
   case 1
     % zap the first compartment
-    V_clamp(:, strcmp(comps, 'Presynaptic1')) = -60;
+    V_clamp(:, strcmp(comps, 'Presynaptic1')) = -70;
     V_clamp(pulseStart:pulseStop, strcmp(comps, 'Presynaptic1')) = pulseHeight;
   case 2
     % zap the second compartment
-    V_clamp(:, strcmp(comps, 'Presynaptic2')) = -60;
+    V_clamp(:, strcmp(comps, 'Presynaptic2')) = -70;
     V_clamp(pulseStart:pulseStop, strcmp(comps, 'Presynaptic2')) = pulseHeight;
   case 3
     % zap both compartments
-    V_clamp(:, strcmp(comps, 'Presynaptic1')) = -60;
+    V_clamp(:, strcmp(comps, 'Presynaptic1')) = -70;
     V_clamp(pulseStart:pulseStop, strcmp(comps, 'Presynaptic1')) = pulseHeight;
-    V_clamp(:, strcmp(comps, 'Presynaptic2')) = -60;
+    V_clamp(:, strcmp(comps, 'Presynaptic2')) = -70;
     V_clamp(pulseStart:pulseStop, strcmp(comps, 'Presynaptic2')) = pulseHeight;
   end
 
