c2828bf3d1bd79226e20dd9cb9fcd8e28a90df2b
Alec Hoyland <alec.hoyland@posteo.net>
2019-05-14 15:53:55 -0400

    Clamp the voltage during steady-state calculation

--- utils/setSteadyState.m
+++ utils/setSteadyState.m
@@ -1,6 +1,7 @@
 function x = setSteadyState(x)
   % compute the steady-state and save as a snapshot
   x.t_end = 5e3;
+  x.V_clamp = [NaN, -70, -70];
   V = x.integrate;
   x.t_end = 30;
   x.snapshot('steady-state');
