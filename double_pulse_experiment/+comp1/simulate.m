72072cf68b4c7f2bed6730943209bec577ae857a
Alec Hoyland <alec.hoyland@posteo.net>
2019-05-15 11:18:43 -0400

    Add documentation, minor fixes

--- double_pulse_experiment/+comp1/simulate.m
+++ double_pulse_experiment/+comp1/simulate.m
@@ -1,4 +1,18 @@
-function [cost, costParts, R, V] = simulate(x, ~, ~)
+function [cost, costParts, response, V] = simulate(x, ~, ~)
+  
+  % function to be passed to the optimizer
+  % Arguments:
+  %   x: the xolotl object
+  %
+  % Outputs:
+  %   cost: 1x1 containing the result of the objective function
+  %   costParts: 4x1 containing four different contributing factors to the cost
+  %     costParts(1): cost due to failing to meet a minimum response height
+  %     costParts(2): cost due to distance from multiplicative target
+  %     costParts(3): cost due to closeness to additive response target
+  %     costParts(4): cost due to failure for response to be in a (supralinear range)
+  %   response: 3x1 containing the response amplitudes for the three trials
+  %   V: n x 3 containing the voltage traces for the three trials
 
   %% Preamble
 
@@ -24,9 +38,9 @@ function [cost, costParts, R, V] = simulate(x, ~, ~)
 
   %% Perform the three simulations
 
-  [R(1), V(:,1)]   = comp1.simulate_core(x, comps, 1, pulseStart, pulseStop, pulseHeight);
-  [R(2), V(:,2)]   = comp1.simulate_core(x, comps, 2, pulseStart, pulseStop, pulseHeight);
-  [R(3), V(:,3)]   = comp1.simulate_core(x, comps, 3, pulseStart, pulseStop, pulseHeight);
+  [response(1), V(:,1)]   = comp1.simulate_core(x, comps, 1, pulseStart, pulseStop, pulseHeight);
+  [response(2), V(:,2)]   = comp1.simulate_core(x, comps, 2, pulseStart, pulseStop, pulseHeight);
+  [response(3), V(:,3)]   = comp1.simulate_core(x, comps, 3, pulseStart, pulseStop, pulseHeight);
 
   [cost, costParts] = costFunction(R, epsilon, lambda);
 
