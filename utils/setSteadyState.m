82382efcf8faa76e5aed12b17629f895263b7ff9
Alec Hoyland <alec.hoyland@posteo.net>
2019-05-17 16:09:37 -0400

    Add second argument

--- utils/setSteadyState.m
+++ utils/setSteadyState.m
@@ -1,8 +1,13 @@
-function x = setSteadyState(x)
+function x = setSteadyState(x, V_clamp)
   % compute the steady-state and save as a snapshot
-  x.t_end = 5e3;
-  x.V_clamp = [NaN, -70, -70];
+  
+  if nargin < 2
+    V_clamp = [NaN, -70, -70];
+  end
+  
+  x.t_end = 5e3;  
+  x.V_clamp = V_clamp;
   V = x.integrate;
-  x.t_end = 30;
+  x.t_end = 100;
   x.snapshot('steady-state');
 end % function
