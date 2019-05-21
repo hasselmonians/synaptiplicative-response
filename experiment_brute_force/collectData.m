a76986a61d2e26adf736e4cfe00adf6438f1a2bf
Alec Hoyland <alec.hoyland@posteo.net>
2019-05-21 14:11:44 -0400

    Add script to collect data from brute force method

---
+++ experiment_brute_force/collectData.m
@@ -0,0 +1,60 @@
+%% collectData
+% Here, we will brute force the solution space by simulating the double pulse experiment
+% for each synaptic strength in an equidistant grid,
+% and compute the responses to the first, second, and double pulses.
+% The model is specified in |model| and the simulation procedure in |simulate|
+
+%% Instantiate the model
+% The model has 4 parameters, corresponding to the four maximal condutances.
+% Two of the conductances, the maximal condutances of the NMDAergic synapses will be varied.
+
+x = model();
+
+% equidistant grid for synaptic maximal conductances
+gmax = 0:0.1:30;
+
+% responses stored in a gmax x gmax x 3 array
+responses = zeros(length(gmax), length(gmax), 3);
+
+% parameter names
+param_names = x.find('Dendrite*NMDAergic*gmax');
+
+% container for parameter values
+param_values = zeros(2, 1);
+
+for ii = 1:length(gmax)
+  corelib.textbar(ii, length(gmax))
+  for qq = 1:length(gmax)
+    param_values = [gmax(ii); gmax(qq)];
+    x.set(param_names, param_values);
+    responses(ii, qq, :) = simulate(x);
+  end
+end
+
+%% Responses as a function of synaptic weights
+
+figure('outerposition',[100 100 1000 800],'PaperUnits','points','PaperSize',[1000 800]); hold on
+imagesc(gmax, gmax, responses(:, :, 3));
+xlabel('g_{max} (\mu S)')
+ylabel('g_{max} (\mu S)')
+title('responses')
+c = colorbar;
+c.Label.String = 'Response height (mV)';
+
+figure('outerposition',[100 100 1000 800],'PaperUnits','points','PaperSize',[1000 800]); hold on
+imagesc(gmax, gmax, responses(:, :, 3) - responses(:, :, 1) .* responses(:, :, 2));
+xlabel('g_{max} (\mu S)')
+ylabel('g_{max} (\mu S)')
+title('multiplicative difference')
+c = colorbar;
+c.Label.String = 'Response height (mV)';
+
+figure('outerposition',[100 100 1000 800],'PaperUnits','points','PaperSize',[1000 800]); hold on
+imagesc(gmax, gmax, responses(:, :, 3) - responses(:, :, 1) - responses(:, :, 2));
+xlabel('g_{max} (\mu S)')
+ylabel('g_{max} (\mu S)')
+title('additive difference')
+c = colorbar;
+c.Label.String = 'Response height (mV)';
+
+save('responses.mat', 'responses', 'param_names', 'param_values', 'x', 'gmax');
--- experiment_brute_force/simulate.m
+++ experiment_brute_force/simulate.m
@@ -17,8 +17,8 @@ function [responses] = simulate(x)
   pulseHeight = 60;
   pulseStart  = round(2 / x.dt);
   pulseStop   = pulseStart + pulseWidth;
-  pulse       = -70 * ones(x.t_end / x.dt);
-  pulse(pulseStart:pulseStop) = pulseHeight;
+  pulse       = -70 * ones(x.t_end / x.dt, 1);
+  pulse(pulseStart:pulseStop, 1) = pulseHeight;
   
   % perform the three simulations
 
