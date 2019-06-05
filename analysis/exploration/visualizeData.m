e9c55d73849fe1c1e6a5560b5569574860eb4ef8
Alec Hoyland <alec.hoyland@posteo.net>
2019-06-05 16:10:18 -0400

    Add excitation section

--- analysis/exploration/visualizeData.m
+++ analysis/exploration/visualizeData.m
@@ -102,7 +102,7 @@ figlib.pretty();
 pdflib.snap();
 delete(gcf)
 
-%% 1-compartment passive case
+%% 1-compartment passive case (double-pulse)
 % In the 1-compartment case, a single compartment representing a cylindrical patch of membrane
 % is postsynaptic to two disconnected presynaptic compartments by NMDAergic synapses.
 % In the first experiment, only passive leak channels were included, aside from the synapses.
@@ -129,6 +129,39 @@ end
 
 try
   dataTable = processData('comp1-passive', 'experiment_double_pulse');
+  [~, I] = sort(dataTable.responses(:,3));
+  plotExemplars('comp1-passive', I(end-9:end));
+catch
+end
+
+%% 1-compartment passive case (excitation)
+% In the 1-compartment case, a single compartment representing a cylindrical patch of membrane
+% is postsynaptic to two disconnected presynaptic compartments by AMPAergic and NMDAergic synapses.
+% The AMPAergic presynaptic cell is stimulated to raise the baseline membrane potential of the postsynaptic cell.
+% Then, the NMDAergic presynaptic cell is stimulated.
+% The three conditions are AMPAergic, NMDAergic and both together.
+
+try
+  displayDataSummary('comp1-passive', 'experiment_excitation', [], true);
+catch
+  disp('no data available')
+end
+
+%%
+% These are the top ten models by lowest cost.
+
+try
+  plotExemplars('comp1-passive', 'experiment_excitation', 10);
+  pdflib.snap
+  delete(gcf)
+catch
+end
+
+%%
+% These are the top ten models by highest response #3.
+
+try
+  dataTable = processData('comp1-passive', 'experiment_excitation');
   [~, I] = sort(dataTable.responses(:,3));
   plotExemplars('comp1-passive', I(end-9:end));
 catch
