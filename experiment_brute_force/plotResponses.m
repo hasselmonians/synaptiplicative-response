ef312e8465b769e69232d43a20cb08919c1c7ff2
Alec Hoyland <alec.hoyland@posteo.net>
2019-06-20 11:52:50 -0400

    Don't plot the current

--- experiment_brute_force/plotResponses.m
+++ experiment_brute_force/plotResponses.m
@@ -8,15 +8,15 @@ function plotResponses(x)
   %     expects one ready to go into the simulate function
 
   % perform the computation and save the three responses and the pulse waveform
-  [~, V, pulse, Isyn] = simulate(x);
+  [~, V, pulse] = simulate(x);
 
   %% Generate the figure to plot on
 
   % don't bother generating multiple plots on one figure
   f = figure('outerposition',[100 100 1000 800],'PaperUnits','points','PaperSize',[1000 800]); hold on
 
-  for ii = 3:-1:1
-    ax(ii) = subplot(3, 1, ii); hold on
+  for ii = 2:-1:1
+    ax(ii) = subplot(2, 1, ii); hold on
   end
 
   time = x.dt * (1:length(pulse));
@@ -33,15 +33,15 @@ function plotResponses(x)
   plot(ax(2), time, pulse, 'k');
 
   % plot the NMDAergic current trace
-  for ii = 1:2
-    plot(ax(3), time, Isyn(:, ii), 'Color', C(ii+1, :));
-  end
+  % for ii = 1:2
+  %   plot(ax(3), time, Isyn(:, ii), 'Color', C(ii+1, :));
+  % end
 
   % add the axis labels
   ylabel(ax(1), 'responses (mV)')
   ylabel(ax(2), 'pulse (mV)')
-  ylabel(ax(3), 'current (nA)')
-  xlabel(ax(3), 'time (ms)')
+  % ylabel(ax(3), 'current (nA)')
+  xlabel(ax(2), 'time (ms)')
 
   % add the legend
   legend(ax(1), leg);
--- experiment_brute_force/simulate.m
+++ experiment_brute_force/simulate.m
@@ -1,4 +1,4 @@
-function [responses, V, pulse, Isyn] = simulate(x)
+function [responses, V, pulse] = simulate(x)
   % this function is used to simulate the voltage response amplitude
   % of two pulses, and then both together, to check for supralinearity
 
@@ -16,7 +16,7 @@ function [responses, V, pulse, Isyn] = simulate(x)
   % preallocate output variables
   responses = zeros(3,1);
   V         = zeros(x.t_end / x.dt, 3);
-  Isyn      = zeros(x.t_end / x.dt, 2);
+  % Isyn      = zeros(x.t_end / x.dt, 2);
 
   % useful variables
   comps     = x.find('compartment');
@@ -67,6 +67,6 @@ function [response, V] = simulate_core(x, comps, index, pulse)
 
   % organize the current trace
   % indices 3 and 6 represent the actual current in nanoamps
-  Isyn = Isyn_raw(:, [3, 6]);
+  % Isyn = Isyn_raw(:, [3, 6]);
 
 end % function
