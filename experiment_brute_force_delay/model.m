a99da92af65571c999c277cc753ba79f5d697dde
Alec Hoyland <alec.hoyland@posteo.net>
2019-07-02 13:30:28 -0400

    Add delay experiments

---
+++ experiment_brute_force_delay/model.m
@@ -0,0 +1,26 @@
+function x = model()
+
+  x = xolotl;
+
+  %% Instantiate compartments and conductances
+
+  x.add('compartment', 'Dendrite', 'Cm', 30, 'radius', 4e-3, 'len', 110e-3);
+
+  % add only a leak current
+  x.Dendrite.add('Leak', 'gbar', 0.1, 'E', -90);
+
+  %% Add presynaptic compartments
+
+  x.add('compartment', 'Presynaptic1', 'Cm', 30, 'radius', 4e-3, 'len', 110e-3);
+  x.add('compartment', 'Presynaptic2', 'Cm', 30, 'radius', 4e-3, 'len', 110e-3);
+
+  % add only a leak current
+  x.Presynaptic1.add('Leak', 'gbar', 0.1, 'E', -90);
+  x.Presynaptic2.add('Leak', 'gbar', 0.1, 'E', -90);
+
+  %% Connect presynaptic compartments to postsynaptic compartments
+
+  x.connect('Presynaptic1', 'Dendrite', 'borgers/NMDAergic', 'gmax', 0, 'Mg', 2, 'tau_d', 30, 'tau_r', 2, 'E', 0);
+  x.connect('Presynaptic2', 'Dendrite', 'borgers/NMDAergic', 'gmax', 0, 'Mg', 2, 'tau_d', 30, 'tau_r', 2, 'E', 0);
+
+end
---
+++ experiment_brute_force_delay/plotResponses.m
@@ -0,0 +1,54 @@
+function plotResponses(x, delay)
+
+  % plots the responses to the three pulse conditions
+  % as response traces vs. time
+
+  % Arguments:
+  %   x: xolotl object that is already set up
+  %     expects one ready to go into the simulate function
+
+  % perform the computation and save the three responses and the pulse waveform
+  [~, V, pulse] = simulate(x, delay);
+
+  %% Generate the figure to plot on
+
+  % don't bother generating multiple plots on one figure
+  f = figure('outerposition',[100 100 1000 800],'PaperUnits','points','PaperSize',[1000 800]); hold on
+
+  for ii = 2:-1:1
+    ax(ii) = subplot(2, 1, ii); hold on
+  end
+
+  time = x.dt * (1:length(pulse));
+  C = colormaps.linspecer(3);
+
+  % plot the three responses
+  leg = cell(3,1);
+  for ii = 1:3
+    plot(ax(1), time, V(:, ii), 'Color', C(ii, :))
+    leg{ii} = ['V_' num2str(ii)];
+  end
+
+  % plot the waveform pulse
+  plot(ax(2), time, pulse(:,1), 'k');
+  plot(ax(2), time, pulse(:,1), 'r');
+
+  % plot the NMDAergic current trace
+  % for ii = 1:2
+  %   plot(ax(3), time, Isyn(:, ii), 'Color', C(ii+1, :));
+  % end
+
+  % add the axis labels
+  ylabel(ax(1), 'responses (mV)')
+  ylabel(ax(2), 'pulse (mV)')
+  % ylabel(ax(3), 'current (nA)')
+  xlabel(ax(2), 'time (ms)')
+
+  % fix axis limits
+  ylim(ax(1), [-95, -65])
+
+  % add the legend
+  legend(ax(1), leg);
+
+  % make the figure pretty
+  figlib.pretty('PlotBuffer', 0.2)
