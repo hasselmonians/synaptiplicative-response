f76dc901f08ac425d155461b6d801700d99b668b
Alec Hoyland <alec.hoyland@posteo.net>
2019-07-02 17:11:39 -0400

    Plot responses to selected delay values

---
+++ experiment_brute_force_delay/plotDelay.m
@@ -0,0 +1,45 @@
+%% Plot some responses as functions of the delay term
+% delay is introduced during the third trial
+% when both presynaptic cells are stimulated
+% the first cell is stimulated at t = pulseStart
+% and the second at t = pulseStart + pulseDelay
+
+%% Create a general model
+
+x = model();
+x.set('*gmax', 30); % this seems to be a good value
+
+% create a vector of delays to test (in ms)
+delay = logspace(0, log10(300), 11);
+
+%% Perform the main loop
+% plot the responses vs. time traces
+% and the pulses
+
+% output vectors
+responses = zeros(length(delay), 1);
+
+for ii = 1:length(delay)
+  % compute the response and pulse
+  [rr, V, pulse] = simulate(x, delay(ii));
+  responses(ii) = rr(3);
+  % plot the response vs. time traces
+  plotResponses(x, delay(ii), V, pulse);
+end
+
+%% Plot the response height as a function of delay
+f = figure('outerposition',[100 100 1000 800],'PaperUnits','points','PaperSize',[1000 800]); hold on
+
+for ii = 2:-1:1
+  ax(ii) = subplot(1, 2, ii);
+end
+
+plot(ax(1), delay, responses, 'ok');
+semilogx(ax(2), delay, responses, 'ok');
+
+xlabel(ax(1), 'delay (ms)')
+xlabel(ax(2), 'log delay')
+ylabel(ax(1), 'response height (mV)')
+title('response height as function of delay')
+
+figlib.pretty('PlotBuffer', 0.2);
