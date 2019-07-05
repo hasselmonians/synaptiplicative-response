6e393b616b050c282901ba73b5b37007f4d811b3
Alec Hoyland <alec.hoyland@posteo.net>
2019-07-05 15:45:32 -0400

    Clean up script

--- experiment_brute_force_delay/visualizeData.m
+++ experiment_brute_force_delay/visualizeData.m
@@ -1,18 +1,45 @@
-% script that makes a bunch of plots to try to make sense of data
+% plot the maximal EPSP response as a function of delay between presynaptic pulses
+% parametrized by maximal synaptic conductance
 
-% canonical model
+% maximal synaptic conductance (μS)
+gmaxes = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60];
+% container for responses (mV)
+responses = cell(length(gmaxes), 1);
+% container for legend entries
+leg = cell(length(gmaxes), 1);
+
+% generate the model
 x = model();
-x.set('*gmax', 30);
-plotDelay(x);
 
-return
+for ii = 1:length(gmaxes)
+  corelib.textbar(ii, length(gmaxes))
+  x.set('*gmax', gmaxes(ii));
+  [responses{ii}, delay] = plotDelay(x, true);
+end
 
-% weaker synaptic connection
-x = model();
-x.set('*gmax', 15);
-plotDelay(x);
+f = figure('outerposition',[100 100 1000 800],'PaperUnits','points','PaperSize',[1000 800]); hold on
+c = colormaps.linspecer(length(gmaxes));
 
-% stronger synaptic connection
-x = model();
-x.set('*gmax', 60);
-plotDelay(x);
+for ii = 2:-1:1
+  ax(ii) = subplot(1, 2, ii); hold on
+end
+
+for ii = 1:length(gmaxes)
+  plot(ax(1), delay, responses{ii}, 'o', 'Color', c(ii, :));
+  plot(ax(2), delay, responses{ii} ./ responses{ii}(1), 'o', 'Color', c(ii, :));
+  % leg{ii} = ['\bar{g} = ' num2str(gmaxes(ii)) ' \muS/mm^2'];
+end
+
+xlabel(ax(1), 'delay (ms)')
+xlabel(ax(2), 'delay (ms)')
+ylabel(ax(1), 'response height (mV)')
+ylabel(ax(2), 'norm. response height')
+% legend(ax(2), leg, 'Location', 'NorthEast')
+
+C = colorbar(ax(2), 'Location', 'EastOutside');
+C.Label.String = 'synaptic strength (\muS/mm^2)';
+C.Ticks = gmaxes;
+caxis(ax(2), [min(gmaxes) max(gmaxes)]);
+colormap(ax(2), colormaps.linspecer)
+
+figlib.pretty('PlotBuffer', 0.2)
