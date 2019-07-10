414a340889b4f844cc67219f92b485c9841b8299
Alec Hoyland <alec.hoyland@posteo.net>
2019-07-10 16:29:56 -0400

    Dynamically set baseline

--- utils/setSteadyState.m
+++ utils/setSteadyState.m
@@ -1,7 +1,7 @@
 function x = setSteadyState(x, V_clamp)
   % compute the steady-state and save as a snapshot
 
-  if nargin < 2
+  if ~exist('V_clamp', 'var')
     V_clamp = [NaN, -90, -90];
   end
 
@@ -9,7 +9,7 @@ function x = setSteadyState(x, V_clamp)
   x.V_clamp = V_clamp;
   V = x.integrate;
   x.t_end = 500;
-  x.set('*V', -90);
+  x.set('*V', min(V_clamp));
   x.V_clamp = [NaN, NaN, NaN];
   x.snapshot('steady-state');
 end % function
