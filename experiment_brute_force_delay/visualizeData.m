50da4fa313d71c653b3cfe6d3568b5d4a6e566ca
Alec Hoyland <alec.hoyland@posteo.net>
2019-07-10 17:31:10 -0400

    Parametrize by delay time

--- experiment_brute_force_delay/visualizeData.m
+++ experiment_brute_force_delay/visualizeData.m
@@ -56,33 +56,46 @@ x.set('*Leak.E', -70);
 % set the synaptic strength of the second presynaptic compartment to 30 μS
 x.Dendrite.NMDAergicPresynaptic2.gmax = 30;
 
-% fix the delay at some constant value
-delay       = 100; % ms
+% take measurements at several delays
+delays = linspace(0, 300, 11);
 % consider a spike an event that happens at -50 mV
 threshold   = -50; % mV
+
 % containers for output variables
 responses   = zeros(length(gmaxes), 1); % mV, only third (combined) response
 crossings   = zeros(length(gmaxes), 1); % will be in ms eventually
 pulseDelays = zeros(length(gmaxes), 2); % will be in ms eventually
 
-% perform the simulation to acquire the responses, crossings, and pulse delays
-for ii = 1:length(gmaxes)
-  corelib.textbar(ii, length(gmaxes))
-  % set the synaptic strength of the first presynaptic compartment
-  x.Dendrite.NMDAergicPresynaptic1.gmax = gmaxes(ii);
-  [responses(ii), ~, crossings(ii), pulseDelays(ii, :)] = plotDelay(x, threshold, delay, true);
-end
-
-% scale the crossings and pulse delays to milliseconds
-crossings   = x.dt * crossings; % ms
-pulseDelays = x.dt * pulseDelays; % ms
-
 % plot crossings measuered from end of first pulse as a function of synaptic strength
 f = figure('outerposition',[100 100 1000 800],'PaperUnits','points','PaperSize',[1000 800]); hold on
+c = colormaps.linspecer(length(delays));
 
-plot(gmaxes, crossings - pulseDelays(:,1), 'ok');
+% perform the simulation to acquire the responses, crossings, and pulse delays for each delay
+len_gmaxes = length(gmaxes);
+len_delays = length(delays);
+for qq = 1:len_delays
+  corelib.textbar((qq-1) * len_gmaxes + ii , len_gmaxes*len_delays)
+  for ii = 1:len_gmaxes
+    % set the synaptic strength of the first presynaptic compartment
+    x.Dendrite.NMDAergicPresynaptic1.gmax = gmaxes(ii);
+    [responses(ii), ~, crossings(ii), pulseDelays(ii, :)] = plotDelay(x, threshold, delays(qq), true);
+  end % ii
+
+  % scale the crossings and pulse delays to milliseconds
+  crossings   = x.dt * crossings; % ms
+  pulseDelays = x.dt * pulseDelays; % ms
+
+  % plot the crossings vs synaptic strength, color based on delay time
+  plot(gmaxes, crossings - pulseDelays(:,1), 'o', 'Color', c(qq, :));
+end % qq
 
 xlabel('synaptic strength (\muS/mm^2)')
 ylabel('threshold crossing time (ms)')
+
+C = colorbar('Location', 'EastOutside');
+C.Label.String = 'delay time (ms)';
+C.Ticks = delays;
+caxis([min(delays) max(delays)]);
+colormap(colormaps.linspecer)
 
 figlib.pretty('PlotBuffer', 0.2)
