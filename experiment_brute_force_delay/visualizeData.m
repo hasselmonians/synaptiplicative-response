523fedff8f4694a90411d89ff880dbc40111a2a2
Alec Hoyland <alec.hoyland@posteo.net>
2019-07-10 14:54:59 -0400

    Compute the threshold crossing times as synaptic strength increases

--- experiment_brute_force_delay/visualizeData.m
+++ experiment_brute_force_delay/visualizeData.m
@@ -18,7 +18,8 @@ for ii = 1:length(gmaxes)
   [responses{ii}, delay] = plotDelay(x, true);
 end
 
-% plot the responses and normalized responses as circles
+%% Plot the responses and normalized responses as circles
+
 f = figure('outerposition',[100 100 1000 800],'PaperUnits','points','PaperSize',[1000 800]); hold on
 c = colormaps.linspecer(length(gmaxes));
 
@@ -45,3 +46,33 @@ caxis(ax(2), [min(gmaxes) max(gmaxes)]);
 colormap(ax(2), colormaps.linspecer)
 
 figlib.pretty('PlotBuffer', 0.2)
+
+%% Plot the threshold crossing times as a function of presynaptic strength
+
+% generate model
+% x = model();
+% set the leak current reversal potential to -70 mV
+x.set('*Leak.gbar', -70);
+% set the synaptic strength of the second presynaptic compartment to 30 μS
+x.Dendrite.NMDAergicPresynaptic2.gmax = 30;
+
+% fix the delay at some constant value
+delay       = 100; % ms
+% consider a spike an event that happens at -50 mV
+threshold   = -50; % mV
+% containers for output variables
+responses   = zeros(length(gmaxes), 1); % mV, only third (combined) response
+crossings   = zeros(length(gmaxes), 1); % will be in ms eventually
+pulseDelays = zeros(length(gmaxes), 2); % will be in ms eventually
+
+% perform the simulation to acquire the responses, crossings, and pulse delays
+for ii = 1:length(gmaxes)
+  corelib.textbar(ii, length(gmaxes))
+  % set the synaptic strength of the first presynaptic compartment
+  x.Dendrite.NMDAergicPresynaptic1.gmax = gmaxes(ii);
+  [responses(ii), ~, crossings(ii), pulseDelays(ii, :)] = plotDelay(x, threshold, delay, true)
+end
+
+% scale the crossings and pulse delays to milliseconds
+crossings   = x.dt * crossings; % ms
+pulseDelays = x.dt * pulseDelays; % ms
