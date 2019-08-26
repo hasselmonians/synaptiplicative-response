803e3db5f7432e0ab6dd0c790e8662b6384b934a
Alec Hoyland <alec.hoyland@posteo.net>
2019-08-26 14:11:49 -0400

    Use set functions by default (to enable parallel processing)

--- utils/setSteadyState.m
+++ utils/setSteadyState.m
@@ -5,11 +5,15 @@ function x = setSteadyState(x, V_clamp)
     V_clamp = [NaN, -90, -90];
   end
 
+  x.set('t_end', 5e3);
   x.t_end = 5e3;
-  x.V_clamp = V_clamp;
+  % x.V_clamp = V_clamp;
+  x.set('V_clamp', V_clamp);
   V = x.integrate;
-  x.t_end = 500;
+  % x.t_end = 500;
+  x.set('t_end', 500);
   x.set('*V', min(V_clamp));
-  x.V_clamp = [NaN, NaN, NaN];
+  % x.V_clamp = [NaN, NaN, NaN];
+  x.set('V_clamp', NaN(1,3));
   x.snapshot('steady-state');
 end % function
