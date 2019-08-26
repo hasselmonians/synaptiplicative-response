f897fe06da66599ba02161d88258d033c87d985b
Alec Hoyland <alec.hoyland@posteo.net>
2019-08-26 16:48:58 -0400

    Use set functions

--- utils/setSteadyState.m
+++ utils/setSteadyState.m
@@ -3,17 +3,16 @@ function x = setSteadyState(x, V_clamp)
 
   if ~exist('V_clamp', 'var')
     V_clamp = [NaN, -90, -90];
+  elseif isscalar(V_clamp)
+    V_clamp = [NaN, V_clamp, V_clamp];
+  else
+    % assume V_clamp has the correct dimensions
   end
 
   x.set('t_end', 5e3);
-  x.t_end = 5e3;
-  % x.V_clamp = V_clamp;
   x.set('V_clamp', V_clamp);
-  V = x.integrate;
-  % x.t_end = 500;
   x.set('t_end', 500);
   x.set('*V', min(V_clamp));
-  % x.V_clamp = [NaN, NaN, NaN];
   x.set('V_clamp', NaN(1,3));
   x.snapshot('steady-state');
 end % function
